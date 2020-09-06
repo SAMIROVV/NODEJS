f378e7cee13857eb2bcfad7d95da1489f482dae0
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-06 23:49:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 //🌷INTEREST BY REGIONS
 
 app.get('/GTIBR', (req, res) => {
-  googleTrends.interestByRegion ({keyword: req.query.keyword, startTime: new Date(Date.now() - (req.query.startTime * 60 * 60 * 1000)),endTime: new Date(Date.now() - (req.query.endTime * 60 * 60 * 1000)), geo: req.query.geo, resolution:req.query.resolution, hl: req.query.language, category: req.query.category, property: req.query.engine})
+  googleTrends.interestByRegion ({keyword: req.query.keyword, startTime: new Date(Date.now() - (req.query.startTime * 60 * 60 * 1000)),endTime: new Date(Date.now() - (req.query.endTime * 60 * 60 * 1000)), geo: req.query.geo, resolution:req.query.resolution, hl: req.query.language, category: req.query.category, property: req.query.engine, resolution: req.query.resolution})
 
         .then(function(results){
         res
