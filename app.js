3113582b6ea34367082d9080a9463ecda4bfb121
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-06 17:31:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const app = express();
 //🌷INTEREST BY REGIONS
 
 app.get('/GTIBR', (req, res) => {
-  googleTrends.interestByRegion ({keyword: req.query.keyword, startTime: new Date(Date.now() - (req.query.startTime * 60 * 60 * 1000)),endTime: new Date(Date.now() - (req.query.endTime * 60 * 60 * 1000)), geo: req.query.geo, resolution:req.query.resolution, hl: req.query.language, category: req.query.category})
-
+  //googleTrends.interestByRegion ({keyword: req.query.keyword, startTime: new Date(Date.now() - (req.query.startTime * 60 * 60 * 1000)),endTime: new Date(Date.now() - (req.query.endTime * 60 * 60 * 1000)), geo: req.query.geo, resolution:req.query.resolution, hl: req.query.language, category: req.query.category})
+googleTrends.interestByRegion ({keyword: req.query.keyword})
         .then(function(results){
         res
             .status(200)
@@ -58,8 +58,9 @@ app.get('/GTIOT', (req, res) => {
 //🌷RELATED QUERIES
 
 app.get('/GTRQ', (req, res) => {
-  googleTrends.relatedQueries ({keyword: req.query.keyword, startTime: new Date(Date.now() - (req.query.startTime * 60 * 60 * 1000)), endTime: new Date(Date.now() - (req.query.endTime * 60 * 60 * 1000)), geo: req.query.geo, hl: req.query.language, category: req.query.category})
-        .then(function(results){
+  //googleTrends.relatedQueries ({keyword: req.query.keyword, startTime: new Date(Date.now() - (req.query.startTime * 60 * 60 * 1000)), endTime: new Date(Date.now() - (req.query.endTime * 60 * 60 * 1000)), geo: req.query.geo, hl: req.query.language, category: req.query.category})
+        googleTrends.relatedQueries ({keyword: req.query.keyword})
+	  .then(function(results){
         res
             .status(200)
             .header('Content-Type', 'application/json')
