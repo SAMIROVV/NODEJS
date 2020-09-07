26c0ea5b8bc359f20331a92a42ff72ae554d7195
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-08 00:59:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -222,7 +222,7 @@ axios.get(req.query.url)
   const $ = cheerio.load(data);
   res
             .status(200)
-            //.header('Content-Type', 'application/json')
+            .header('Content-Type', 'application/json')
 	
             .send(data)
             .end();
