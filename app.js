0f35f7867f0bb3517f2246ff605bb43aa8e66d7b
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-13 21:56:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ app.get('/GTRTT', (req, res) => {
 
 //🐘🐘google trends Widgets token
 
-app.get('/GTTOKN', (req1, res1) => {
+app.get('/GTTOKN', (req, res1) => {
 
 	const request = require('request');
 
@@ -168,7 +168,7 @@ const options = {
     url: 'https://scraper.nepochataya.pp.ua/sites',
     json: true,
     //body: {"url":"https://trends.google.com/trends/api/explore?hl=en-US&tz=-60&req=%7B%22comparisonItem%22:%5B%7B%22keyword%22:%22bitcoin%22,%22geo%22:%22%22,%22time%22:%22today+5-y%22%7D%5D,%22category%22:0,%22property%22:%22youtube%22%7D&tz=-60","request":{"headers":{"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36"}}}
-    body: req1.query.url,
+    body: req.query.url
 };
 
 request.post(options, (err, res, body) => {
