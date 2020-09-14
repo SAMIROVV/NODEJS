c5972fb75b5f3e5e02c58d1ee4b1d8c098c9ad39
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-14 13:10:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ const options = {
     url: 'https://scraper.nepochataya.pp.ua/sites',
     json: true,
     //body: {"url":"https://trends.google.com/trends/api/explore?hl=en-US&tz=-60&req=%7B%22comparisonItem%22:%5B%7B%22keyword%22:%22bitcoin%22,%22geo%22:%22%22,%22time%22:%22today+5-y%22%7D%5D,%22category%22:0,%22property%22:%22youtube%22%7D&tz=-60","request":{"headers":{"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36"}}}
-body: {"url": req.query.path+'&tz='+req.query.tz+'&req='+req.query.req}
+body: {"url": req.query.path+'&tz='+req.query.tz+'&req='+req.query.reqy}
 
 };
 
@@ -179,7 +179,7 @@ request.post(options, (err, res, body) => {
     }
     //console.log(`Status: ${res.statusCode}`);
     console.log(body);
-	console.log('resultttts', req.query.path+'&tz='+req.query.tz+'&req='+req.query.req);
+	console.log('resultttts', req.query.path+'&tz='+req.query.tz+'&req='+req.query.reqy);
 		 
 		
 	
