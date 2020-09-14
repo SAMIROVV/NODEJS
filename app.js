511197898a8e7f0e9b37d07e35edc09e03823c93
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-14 01:13:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -168,8 +168,7 @@ app.get('/GTTOKN', (req, res1) => {
 const options = {
     url: 'https://scraper.nepochataya.pp.ua/sites',
     json: true,
-    body: {"url":%22https://trends.google.com/trends/api/explore?hl=en-US&tz=-60&req=%7B%22comparisonItem%22:%5B%7B%22keyword%22:%22bitcoin%22,%22geo%22:%22%22,%22time%22:%22today+5-y%22%7D%5D,%22category%22:0,%22property%22:%22youtube%22%7D&tz=-60%22,%22request%22:{%22headers%22:{%22User-Agent%22:%22Mozilla/5.0%20(Windows%20NT%206.1;%20WOW64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/40.0.2214.115%20Safari/537.36%22}}}
-	
+    body: {"url":"https://trends.google.com/trends/api/explore?hl=en-US&tz=-60&req=%7B%22comparisonItem%22:%5B%7B%22keyword%22:%22bitcoin%22,%22geo%22:%22%22,%22time%22:%22today+5-y%22%7D%5D,%22category%22:0,%22property%22:%22youtube%22%7D&tz=-60","request":{"headers":{"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36"}}}
 };
 
 request.post(options, (err, res, body) => {
