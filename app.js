44079ca45faf1152b6dbafe4f7f06c6b6937d243
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-16 00:33:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -194,7 +194,7 @@ Request(options2, (err, res, body) => {
 var obj = JSON.parse(body.replace(')]}','').replace(/'/g, ''));  
 //console.log(obj.widgets[0].request);
 var urltimeserie = { protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/multiline', query: {hl: req1.query.hl, tz: req1.query.tz} }; 
-var Urlts = url.format(urltimeserie)+'&req='+JSON.parse(obj.widgets[0]).request+'&tz=-60';
+var Urlts = url.format(urltimeserie)+'&req='+JSON.stringify(obj.widgets[0].request)+'&tz=-60';
 		  	
 	
 	const options3 = {
