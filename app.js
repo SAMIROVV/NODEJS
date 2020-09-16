eb83f460d7205ff25bcc7a7fc55d07d462b446e7
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-17 00:32:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,13 +23,11 @@ const Request = require('request');
 		res0.send(body);}
 		
 	if(type === 'GTEXPLORE'){
-		if (err) {res0.send(body);}
 		res0.send(body.replace(')]}','').replace(/'/g, ''));}	
 		
 	if(type === 'GTTS'){
                 Request({url:url.format({ protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/multiline', query: {hl: req0.query.hl, tz: tz, req: JSON.stringify(obj.widgets[0].request), token: obj.widgets[0].token,tz: tz} }), json: true,}, (err, res, body) => {  
-                if (err) {res0.send(body);}
-                else{ res0.send(JSON.parse(body.replace(')]}','').replace(/'/g, '')  ))};});}
+                res0.send(body.replace(')]}','').replace(/'/g, '').replace(',',''));});}
 		
 		
 		
