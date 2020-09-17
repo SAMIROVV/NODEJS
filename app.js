eece35ae9f7f08e1fe09283d58afd608d40f80f9
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-17 21:45:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ const Request = require('request');
 		
 		
 		
-		res0.send(JSON.stringify(body)).end();
+		res0.send(JSON.stringify(body).replace(Math.floor(Date.now() / 1000)+'tfr','')).end();
         });});
 
 
