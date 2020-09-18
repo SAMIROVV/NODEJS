0332ee514636daf577760430027552a1d539e4bd
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-18 14:04:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -192,9 +192,9 @@ const options = {
 };
 	
 	
-	scrape(options, (err, res, body) => {	Promise.resolve(body);
+	scrape(options, (err, res, body) => {	
    //console.log(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER6?',''));console.log('directory', decodeURIComponent(url.format({protocol: 'https',   host: req0.get('host'),    pathname: req0.originalUrl})));
-        //res0.header('Content-Type', 'application/json').send(body).end(); 		
+        res0.send(body).end(); 		
 		
         });});
 	
