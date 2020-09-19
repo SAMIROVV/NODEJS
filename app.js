b8e5e7188c2e6b39a681ae7157ae588c5fe1378b
samirov2 <70457295+samirov2@users.noreply.github.com>
2020-09-19 13:24:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -139,9 +139,9 @@ const Request = require('request');
         var type = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[1];
 		
 	if(type === 'SCPURL' || type === ''){
-		var obj1 = JSON.stringify(body).replace(/\\/g, '').replace('{"Success":true,"Text":")]}','').replace('n{"widgets"','{"widgets').replace(/'/g, '');
-		var obj = JSON.parse(obj1)
-                res0.send(obj);}
+		var obj1 = JSON.parse(body.replace(/\\/g, '').replace('{"Success":true,"Text":")]}','').replace('n{"widgets"','{"widgets').replace(/'/g, ''));
+//var obj = JSON.parse(obj1)
+                res0.send(obj1);}
 		
 	if(type === 'SCPGTACM'){
 		res0.header('Content-Type', 'application/json').send(JSON.stringify(body).replace(/\\/g, ''));}
