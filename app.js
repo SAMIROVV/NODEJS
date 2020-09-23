cce049915122e5eb66ec3c6aef7eea24c5770ae4
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-23 16:29:39 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+                                                      //🔴♼♼♼♼♼      TO SCRAPE WEBSITEZ ESPECIALLY GOOGLE TRENDS      ♼♼♼♼♼🔴//
+
+const express = require('express');
+const app = express(); 
+
+
+//💔 AMAZING GOOGLE TRENDS SCRAPER WITH TOKEN
+
+app.get('/SCRAPER1', (req0, res0) => {
+const https = require('https');
+var options1 = {hostname: 'https://trends.google.com', path: decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER1?https://trends.google.com',''),method: 'GET',};
+
+https.get(options1, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {data += chunk; });
+    res.on('end', () => {
+    var cookie = res.headers['set-cookie'][0].split(';')[0];    
+    var options2 = {hostname: 'trends.google.com', path: decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER1?https://trends.google.com',''), method: 'GET', headers: {'cookie': cookie} };
+       
+https.get(options2, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {data += chunk; });
+    res.on('end', () => {
+             console.log(JSON.parse(data.slice(4)).widgets[1].token);          console.log(JSON.parse(data.slice(4)).widgets[1].request);   });}) });})
+
+});
