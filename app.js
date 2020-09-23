5b44dcb2e73ecbbdd1c68089699496a798ab27f0
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-23 20:17:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.get('/SCRAPER1', (req0, res0) => {
 const https = require('https');
+const url = require('url'); 
 var options1 = {hostname: 'https://trends.google.com', path: decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER1?https://trends.google.com',''),method: 'GET',};
 
 https.get(options1, (res) => {
