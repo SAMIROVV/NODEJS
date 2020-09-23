f73aa85078ee43df9d5f141093817c998e2bc23e
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-24 00:02:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,21 +13,20 @@ const app = express();
 app.get('/SCRAPER0', (req0, res0) => {
     const https = require('https');
     const url = require('url'); 
-    var urlObject = {hostname: 'trends.google.com', path: decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER1?https://trends.google.com',''),method: 'GET'},
+    var options1 = {hostname: 'trends.google.com', path: decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER0?https://trends.google.com',''),method: 'GET'},
 
-https.get(urlObject, (res) => {
+https.get(options1, (res) => {
     let data = '';
     res.on('data', (chunk) => {data += chunk; });
     res.on('end', () => {
     var cookie = res.headers['set-cookie'][0].split(';')[0];    
-    var options2 = {urlObject, headers: {'cookie': cookie} };
+    var options2 = {hostname: 'trends.google.com', path: decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER0?https://trends.google.com',''), method: 'GET', headers: {'cookie': cookie} };
        
 https.get(options2, (res) => {
     let data = '';
     res.on('data', (chunk) => {data += chunk; });
     res.on('end', () => {
-             console.log(JSON.parse(data.slice(4)).widgets[1].token);          
-	    console.log(JSON.parse(data.slice(4)).widgets[1].request);
+            
     
     var type = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[1];
     var tz = req0.query.tz;         
@@ -36,34 +35,20 @@ https.get(options2, (res) => {
 		res0.header('Content-Type', 'application/json').send(data.replace(')]}','').replace(/'/g, ''));}
 	    
 	    
-        if(type === 'SCPGTTS'){
-		
-//const fetch = require('node-fetch');
-		
-                var obj = JSON.parse(data.replace(')]}','').replace(/'/g, ''));   
+        if(type === 'SCPGTTS'){		
                 var options3 = url.format({ protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/multiline', query: {hl: req0.query.hl, tz: tz, req: JSON.stringify(JSON.parse(data.slice(4)).widgets[0].request), token: JSON.parse(data.slice(4)).widgets[0].token,tz: tz} })  
-                
-	
                 https.get(options3, (res) => {  
-                //res0.header('Content-Type', 'application/json').send(body.replace(')]}','').replace(/'/g, '').replace(',',''));
 		let data = '';
-    res.on('data', (chunk) => {data += chunk; });
-    res.on('end', () => {res0.header('Content-Type', 'application/json').send(data.replace(')]}','').replace(/'/g, '').replace(',',''));});
-		
-		
-		});
-	
-	}
+                  res.on('data', (chunk) => {data += chunk; });
+                  res.on('end', () => {res0.header('Content-Type', 'application/json').send(data.replace(')]}','').replace(/'/g, '').replace(',',''));});});}
 		    
     
     
-    });}) });
-    
-
-
-})
-
-});
+    });
+    }); 
+    });
+    });
+    });
 
 
 
