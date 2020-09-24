08731ea1c6d3738a6b5b90ccaafb918c384da33b
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-24 17:41:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ https.get(options2, (res) => {
     }
 		
 		
-		res0.header('Content-Type', 'application/json').send(JSON.parse(data.slice(4)).widgets);console.log(JSON.parse(data.slice(4)).widgets[].token);};   
+		res0.header('Content-Type', 'application/json').send(JSON.parse(data.slice(4)).widgets);};   
 	    
 	    
         if(type === 'SCPGTTS'){		
