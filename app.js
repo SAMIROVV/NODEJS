5872b9fde7fb577e86e41db7369c76dc1f974dff
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-25 03:22:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ https.get(options2, (res) => {
 	if(type === 'SCPGTEXPLORE' || type === ''){
 		
 console.log(JSON.parse(data.slice(4)).widgets.map(function (item) {  return item.id.replace('_0','').replace('_1','').replace('_2','').replace('_3','').replace('_4','')+'■'+JSON.stringify(item.request)+'■'+item.token})   );
-var dat = if(JSON.parse(data.slice(4)).widgets.filter(function(o){    return (o.id === 'GEO_MAP')})==='[Array]'){JSON.STRINGIFY(JSON.parse(data.slice(4)).widgets.filter(function(o){    return (o.id === 'GEO_MAP')}))}else{JSON.parse(data.slice(4)).widgets.filter(function(o){    return (o.id === 'GEO_MAP')})};
-console.log(dat);
+//var dat = if(JSON.parse(data.slice(4)).widgets.filter(function(o){    return (o.id === 'GEO_MAP')})==='[Array]'){JSON.STRINGIFY(JSON.parse(data.slice(4)).widgets.filter(function(o){    return (o.id === 'GEO_MAP')}))}else{JSON.parse(data.slice(4)).widgets.filter(function(o){    return (o.id === 'GEO_MAP')})};
+//console.log(dat);
 		
 
 
