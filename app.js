10b1e17d9e4660171640e1f027cc1f4389d4a41a
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-26 00:07:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -48,9 +48,11 @@ https.get({hostname: 'trends.google.com', path: decodeURIComponent(url.format({ 
     
 	    
 	if(type === 'SCPGTEXPLORE' || type === ''){
-             res0.header('Content-Type', 'application/json').send(JSON.parse(data.slice(4)).widgets);};   
-	    
-	    
+             res0.header('Content-Type', 'application/json').send(JSON.parse(data.slice(4)).widgets);   
+	    console.log(td);
+	    console.log(filterItems(td, 'related_topics■{')[0].split('■')[1]);
+	
+	};
         if(type === 'SCPGTTS'){		
              https.get(url.format({ protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/multiline', query: {hl: req0.query.hl, tz: req0.query.tz[0], req: filterItems(td, 'timeseries■{')[0].split('■')[1], token: filterItems(td, 'timeseries■{')[0].split('■')[2],tz: req0.query.tz[0]} })  , (res) => {  
                 let data = '';
@@ -69,9 +71,13 @@ https.get({hostname: 'trends.google.com', path: decodeURIComponent(url.format({ 
                 https.get(url.format({ protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/comparedgeo', query: {hl: req0.query.hl, tz: req0.query.tz[0], req: filterItems(td, 'related_topics■{')[0].split('■')[1], token: filterItems(td, 'related_topics■{')[0].split('■')[2]  ,tz: req0.query.tz[0]} })  , (res) => {  
                   let data = '';
                   res.on('data', (chunk) => {data += chunk; });
-                  res.on('end', () => {res0.header('Content-Type', 'application/json').send(JSON.parse(data.slice(5)));});});}
+                  res.on('end', () => {res0.header('Content-Type', 'application/json').send(JSON.parse(data.slice(5)));});});
 		    
-	    
+	    console.log(https.get(url.format({ protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/comparedgeo', query: {hl: req0.query.hl, tz: req0.query.tz[0], req: filterItems(td, 'related_topics■{')[0].split('■')[1], token: filterItems(td, 'related_topics■{')[0].split('■')[2]  ,tz: req0.query.tz[0]} })  , (res) => {  
+                 );
+		    }
+				  
+				  
 	if(type === 'SCPGTQRY'){		
                 https.get(url.format({ protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/comparedgeo', query: {hl: req0.query.hl, tz: req0.query.tz[0], req: filterItems(td, 'related_queries■{')[0].split('■')[1], token: filterItems(td, 'related_queries■{')[0].split('■')[2]  ,tz: req0.query.tz[0]} })  , (res) => {  
                   let data = '';
