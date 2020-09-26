5db5d5d3dadc52525f0b012300436acbd8cafe1d
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-26 20:10:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ https.get({hostname: 'trends.google.com', path: decodeURIComponent(url.format({ 
                   res.on('data', (chunk) => {data += chunk; });
                   res.on('end', () => {
 			  var first = JSON.parse(data.slice(5)).default.rankedList[0].rankedKeyword.map(function (item) {  return item.query+'■'+item.value})+'↕'+JSON.parse(data.slice(5)).default.rankedList[1].rankedKeyword.map(function (item) {  return item.query+'■'+item.value}) ;    
-			  if(typeof filterItems(td, 'related_queries■{')[1] === "undefined"){res0.header('Content-Type', 'application/json').send(first);console.log(filterItems(td, 'related_queries■{')[1]);}
+			  if(filterItems(td, 'related_queries■{')[1] === undefined){res0.header('Content-Type', 'application/json').send(first);console.log(filterItems(td, 'related_queries■{')[1]);}
 			     else{
 			
 			     
