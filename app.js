6e02129f6ce2aeded72b4e9585cc9bb9b8a1b22b
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-26 16:47:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ https.get({hostname: 'trends.google.com', path: decodeURIComponent(url.format({ 
                   res.on('end', () => {var first = JSON.parse(data.slice(5)).default.rankedList[0].rankedKeyword.map(function (item) {  return item.topic.title+'■'+item.topic.type+'■'+item.value})+'↕'+JSON.parse(data.slice(5)).default.rankedList[1].rankedKeyword.map(function (item) {  return item.topic.title+'■'+item.topic.type+'■'+item.value}) ;  
 	
 		
-                if(filterItems(td, 'related_topics■{')[1] = 'unde'){res0.send(first);}
+                if(filterItems(td, 'related_topics■{')[1] = 'undefined'){res0.send(first);}
 		   else{
 		   https.get(url.format({ protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/relatedsearches', query: {hl: req0.query.hl, tz: req0.query.tz[0], req: filterItems(td, 'related_topics■{')[1].split('■')[1], token: filterItems(td, 'related_topics■{')[1].split('■')[2]  ,tz: req0.query.tz[0]} })  , (res) => {  
                   let data = '';
@@ -84,10 +84,16 @@ res0.send(first+'🔰'+second);
 				       
 		   
 		   });});
+			   
+			   
+			   }
+				       
+				       
+				       
 			   });});
 		   
 		   
-		   }
+		   
                   
 				       			      
 				      
