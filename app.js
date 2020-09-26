22ddbd8812061c8dda2c309f5193f1670db45d0b
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-26 19:47:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ https.get({hostname: 'trends.google.com', path: decodeURIComponent(url.format({ 
                   res.on('data', (chunk) => {data += chunk; });
                   res.on('end', () => {
 			  var first = JSON.parse(data.slice(5)).default.rankedList[0].rankedKeyword.map(function (item) {  return item.query+'■'+item.value})+'↕'+JSON.parse(data.slice(5)).default.rankedList[1].rankedKeyword.map(function (item) {  return item.query+'■'+item.value}) ;    
-			  if(filterItems(td, 'related_queries■{')[1] = 'undefined'){res0.header('Content-Type', 'application/json').send(first);}
+			  if(filterItems(td, 'related_queries■{')[1] = 'undefined'){res0.header('Content-Type', 'application/json').send(first);console.log(filterItems(td, 'related_queries■{')[1]);}
 			     else{
 			https.get(url.format({ protocol: 'https', hostname: 'trends.google.com/', pathname: 'trends/api/widgetdata/relatedsearches', query: {hl: req0.query.hl, tz: req0.query.tz[0], req: filterItems(td, 'related_queries■{')[1].split('■')[1], token: filterItems(td, 'related_queries■{')[1].split('■')[2]  ,tz: req0.query.tz[0]} })  , (res) => {  
                   let data = '';
