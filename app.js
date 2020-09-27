071e94a36f1510d920554a0d2105ebfb893f4bdb
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-27 20:35:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.get('/SCRAPER0', (req0, res0,res1) => {
                https.get(options0  , (res) => {  
                     let data = '';
                     res.on('data', (chunk) => {data += chunk; });
-                    res.on('end', () => {res0.send(JSON.parse(data.slice(5)).default.trendingSearchesDays[0].trendingSearches.map(function (item) {  return item.title.query+'☉'+item.formattedTraffic+'☉'+item.relatedQueries+'☉'+item.articles.map(function (item) {  return item.title+'🍎'+item.timeAgo+'🍎'+item.snippet}) }));
-                                      });});}  
+                    res.on('end', () => {//res0.send(JSON.parse(data.slice(5)).default.trendingSearchesDays[0].trendingSearches.map(function (item) {  return item.title.query+'☉'+item.formattedTraffic+'☉'+item.relatedQueries+'☉'+item.articles.map(function (item) {  return item.title+'🍎'+item.timeAgo+'🍎'+item.snippet}) }));
+                                    res0.send(data);  });});}  
 	
 	
 	
