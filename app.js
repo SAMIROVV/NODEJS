25c2cdf347d2c5e20b23c8b13b803e55254fa810
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-28 19:35:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -267,7 +267,7 @@ const Request = require('request');
 		
                 //Request.post({url: 'https://scraper.nepochataya.pp.ua/sites', json: true, body: {"url":decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER3?','')  }}, (err, res, body) => {	
         
-		  Request.post('https://scraper.nepochataya.pp.ua/sites').send( {"url":decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER3?','')  }).end(function(err,res ) {	
+		  Request.post('https://scraper.nepochataya.pp.ua/sites').send( {"url":decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER3?','')  }).end(function(err,body ) {	
         
 					      
 					      
@@ -281,7 +281,7 @@ const Request = require('request');
 			
 			
 			
-               https.get('https://scraper.nepochataya.pp.ua'+res.body.previewPath+'/', (res) => {		
+               https.get('https://scraper.nepochataya.pp.ua'+body.previewPath+'/', (res) => {		
                     let data = '';
                     res.on('data', (chunk) => {data += chunk; });
 			res.on('end', () => {					
@@ -292,7 +292,7 @@ const Request = require('request');
 		//TO SCRAPE URL & GTRENDS EXPLORE & AUTOCOMPLETE & DAILY TRENDS & REAL TIME TRENDS
 		
 	if(type === 'SCPURL' || type === ''){
-		res0.header('Content-Type', 'application/json').send(data);console.log(res.body)}
+		res0.header('Content-Type', 'application/json').send(data);console.log(body)}
 				
 		
 	if(type === 'SCPGTACM'){
