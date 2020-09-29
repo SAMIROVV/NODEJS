87df60f5ca6417178b613cf34a73ad2db4f2f081
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-29 20:24:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/SCRAPER0', (req0, res0,res1) => {
                https.get(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER0?','')  , (res) => {  
                     let data = '';
                     res.on('data', (chunk) => {data += chunk; });
-                    res.on('end', () => {res0.send(data.toString().trim())//JSON.parse(data.slice(5).replace(/^\ufeff/g,"")).default.trendingSearchesDays[0].trendingSearches.map(function (item) {  return item.title.query+'☉'+item.formattedTraffic+'☉'+item.relatedQueries.map(function (item) {  return item.query})+'☉'+item.articles.map(function (item) {  return item.title+'🍎'+item.timeAgo+'🍎'+item.snippet})   }));
+                    res.on('end', () => {res0.send(data.charCodeAt(0))//JSON.parse(data.slice(5).replace(/^\ufeff/g,"")).default.trendingSearchesDays[0].trendingSearches.map(function (item) {  return item.title.query+'☉'+item.formattedTraffic+'☉'+item.relatedQueries.map(function (item) {  return item.query})+'☉'+item.articles.map(function (item) {  return item.title+'🍎'+item.timeAgo+'🍎'+item.snippet})   }));
                                       });});}  
 	
 	
