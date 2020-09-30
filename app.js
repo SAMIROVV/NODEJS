7d52ee3de01faf1e7e24eb2656e6065afa6c261a
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-30 14:56:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ app.get('/SCRAPER4', (req0, res0) => {
                 https.get(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER4?','')   , (res) => {  
                     let data = '';
                     res.on('data', (chunk) => {data += chunk; });
-                    res.on('end', () => {res0.header('Content-Type', 'application/json').send(JSON.parse(data).req0.query.keyword[0].trends);
+                    res.on('end', () => {res0.header('Content-Type', 'application/json').send(JSON.parse(data).req0.query.["keyword[]"][0].trends);
 				      });});
 			
 		});
