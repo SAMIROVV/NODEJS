c4e6c858aeb1d737c2c0894e9da3e058fa20d634
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-30 21:07:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -348,7 +348,7 @@ app.get('/SCRAPER4', (req0, res0) => {
                 https.get(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER4?','')   , (res) => {  
                     let data = '';
                     res.on('data', (chunk) => {data += chunk; });
-                    res.on('end', () => {res0.header('Content-Type', 'application/json').send(JSON.parse(data)[keyword1].queries[keyword1].top.map(function (item) {  return item.query+'☉'+item.value})+'↕'+JSON.parse(data)[keyword1].queries[keyword1].rising.map(function (item) {  return item.query+'☉'+item.value}))//JSON.parse(data)[keyword1].trends+'↕'+JSON.parse(data)[keyword1].queries[keyword1].top);
+                    res.on('end', () => {res0.header('Content-Type', 'application/json').send(JSON.parse(data)[keyword1].queries[keyword1].top.map(function (item) {  return item.query+'☉'+item.value})+'↕'+JSON.parse(data)[keyword1].queries[keyword1].rising.map(function (item) {  return item.query+'☉'+item.value})+'💚'+JSON.parse(data)[keyword1].topics.map(function (item) {  return item.topic_title+'☉'+item.topic_type+'☉'+item.value})       )     //JSON.parse(data)[keyword1].trends+'↕'+JSON.parse(data)[keyword1].queries[keyword1].top);
 				      });});
 			
 		});
