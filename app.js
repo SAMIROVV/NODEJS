03cee6c8015e444d29ba9944c284cf97d8515014
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-09-30 16:04:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -335,12 +335,17 @@ const Request = require('request');
 app.get('/SCRAPER4', (req0, res0) => {
     const https = require('https');
     const url = require('url');
-    var keywords = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER4?','').replace('[','').replace(']','');    
+    //var keywords = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER4?','').replace('[','').replace(']','');    
+	var keyword1 = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[1];
+	var keyword2 = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[2];
+			
+	
+	
 	
                 https.get(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/SCRAPER4?','')   , (res) => {  
                     let data = '';
                     res.on('data', (chunk) => {data += chunk; });
-                    res.on('end', () => {res0.header('Content-Type', 'application/json').send(keywords.query.keyword)//send(JSON.parse(data).req0.keyword2.trends);console.log('keywords:',keywords);
+                    res.on('end', () => {res0.header('Content-Type', 'application/json').send(keyword2)//send(JSON.parse(data).req0.keyword2.trends);console.log('keywords:',keywords);
 				      });});
 			
 		});
