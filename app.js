5c52b9924da8e6efedc0254becbcd5a951cb83bb
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-01 03:16:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -336,7 +336,7 @@ app.get('/SCRAPER4', (req0, res0) => {
     const https = require('https');
     const url = require('url');
        
-	var keyword = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[1];
+	var keyword1 = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[1];
 	var keyword2 = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[2];
 	var keyword3 = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[3];
 	var keyword4 = decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[4];
@@ -351,7 +351,7 @@ app.get('/SCRAPER4', (req0, res0) => {
                     //res.on('end', () => {res0.header('Content-Type', 'application/json').send('💚'+JSON.parse(data)[keyword1].queries[keyword1].top.map(function (item) {  return item.query+'☉'+item.value})+'↕'+JSON.parse(data)[keyword1].queries[keyword1].rising.map(function (item) {  return item.query+'☉'+item.value})+'🔰'+   Object.values(JSON.parse(data)[keyword1].topics[0]).map(function (item) {  return item.topic_title+'☉'+item.topic_type+'☉'+item.value})+'↕'+Object.values(JSON.parse(data)[keyword1].topics[1]).map(function (item) {  return item.topic_title+'☉'+item.topic_type+'☉'+item.value})   +'🔰'+Object.values(JSON.parse(data)[keyword1].trends)   .map(function (item) {  return item[keyword1]})      +'🔰'+ Object.keys(JSON.parse(data)[keyword1].region)+'↕'+Object.values(JSON.parse(data)[keyword1].region).map(function (item) {  return item[keyword1]})     )       
 				    
 					 //res.on('end', () => {res0.header('Content-Type', 'application/json').send('💚'+JSON.parse(data)[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].queries[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].top.map(function (item) {  return item.query+'☉'+item.value})+'↕'+JSON.parse(data)[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].queries[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].rising.map(function (item) {  return item.query+'☉'+item.value})+'🔰'+   Object.values(JSON.parse(data)[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].topics[0]).map(function (item) {  return item.topic_title+'☉'+item.topic_type+'☉'+item.value})+'↕'+Object.values(JSON.parse(data)[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].topics[1]).map(function (item) {  return item.topic_title+'☉'+item.topic_type+'☉'+item.value})   +'🔰'+Object.values(JSON.parse(data)[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].trends)   .map(function (item) {  return item[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]]})      +'🔰'+ Object.keys(JSON.parse(data)[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].region)+'↕'+Object.values(JSON.parse(data)[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]].region).map(function (item) {  return item[decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[i]]})     )       
-				res.on('end', () => {console.log('fafa:', length);
+				res.on('end', () => {console.log('fafa:', keyword2);console.log('fafa2:', keyword1);
 		
 		
 					
