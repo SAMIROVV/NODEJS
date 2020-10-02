9dfd9458641d2981c0ffaf1f361d33c446e240c2
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-02 16:10:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -416,10 +416,73 @@ https.get('https://www.youtube.com/results?search_query=goo'   , (res) => {
                     res.on('data', (chunk) => {data += chunk; });
                     res.on('end', () => {
 			    const line = data.match(/window\["ytInitialData"]\s*=\s*(.*);+\n/)[0]
-			    const json = JSON.parse(line.substring(line.indexOf('{'), line.length - 2))
+			    const json = JSON.parse(line.substring(line.indexOf('{'), line.length - 2))			    
 			    const result = json ['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer'] ['contents'][0]['itemSectionRenderer']['contents']
-			    const type = Object.keys(result)[0].replace('Renderer', '')
-			    res0.header('Content-Type', 'application/json').send(result);
+			    
+			    const kind = result.filter(video => {
+        const type = Object.keys(video)[0].replace('Renderer', '')
+        if ('video' === 'video') return type === 'video'
+        else if ('playlis' === 'playlist') return type === 'playlist'
+        else return ['video', 'playlist'].includes(type)   })
+.map(video => {
+        const type = Object.keys(video)[0].replace('Renderer', '')
+        const data = video[type + 'Renderer']
+        const identifier = data[type + 'Id']
+        if (type === 'video') {
+            const isStream = !Object.keys(data).includes('lengthText')
+            let length = Number.MAX_VALUE
+            if (!isStream) {
+                length = 0
+                data['lengthText']['simpleText'].split(':').reverse().forEach((value, index) => {
+                    const i = Number(value)
+                    length += (index === 0 ? i : i * (60 ** index))
+                })
+            }
+            return {
+                type: type,
+                identifier: identifier,
+                uri: 'https://www.youtube.com/watch?v=' + identifier,
+                title: data['title']['runs'][0]['text'],
+                author: {
+                    name: data['ownerText']['runs'][0]['text'],
+                    profile: data['channelThumbnailSupportedRenderers']['channelThumbnailWithLinkRenderer']
+                        ['thumbnail']['thumbnails'][0]['url'],
+                    uri: 'https://www.youtube.com' + data['ownerText']['runs'][0]['navigationEndpoint']
+                        ['commandMetadata']['webCommandMetadata']['url']
+                },
+                length: {
+                    ms: isStream ? length : length * 1000,
+                    sec: length
+                },
+                isStream: isStream,
+                thumbnails: data['thumbnail']['thumbnails']
+            }
+        } else return {
+            type: type,
+            identifier: identifier,
+            uri: 'https://www.youtube.com/playlist?list=' + identifier,
+            title: data['title']['simpleText'],
+            author: {
+                name: data['longBylineText']['runs'][0]['text'],
+                uri: 'https://www.youtube.com' + data['longBylineText']['runs'][0]['navigationEndpoint']
+                    ['commandMetadata']['webCommandMetadata']['url']
+            },
+            count: Number(data['videoCount']),
+            thumbnails: data['thumbnails']
+        }
+    })
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    res0.header('Content-Type', 'application/json').send(kind);
 				      });});
 	
 });
