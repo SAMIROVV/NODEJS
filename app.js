025458bcecca6db3eba89d96206e78c8ea1c74bc
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-02 19:56:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -481,7 +481,7 @@ duration :  data['lengthText'],//['simpleText'],
 			      
 			    
 			    
-			    res0.header('Content-Type', 'application/json').send(kind.map(function (item) {  return 
+			    res0.header('Content-Type', 'application/json').send(JSON.parse(kind).map(function (item) {  return 
 								 item.type+'☉'+item.identifier+'☉'+item.uri+'☉'+
 								 item.title+'☉'+item.author+'☉'+item.thumbnails.url+'☉'+item.description
 				                                 +'☉'+item.publishedTime+'☉'+item.viewCount+'☉'+item.duration})
