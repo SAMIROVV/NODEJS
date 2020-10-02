3ed3f206e6e9e95e47b5796e3a9096ad2ff84bd1
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-02 23:29:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -449,7 +449,7 @@ https.get(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("
                 uri: 'https://www.youtube.com/watch?v=' + identifier,
                 title: data['title']['runs'][0]['text'],
 description : data['descriptionSnippet']['runs'][0]['text'],
-publishedTime: data['publishedTimeText'],//.simpleText,
+publishedTime: data['publishedTimeText'][0]['simpleText'],
 viewCount : data['viewCountText'],//.simpleText,   
 duration :  data['lengthText'],//.simpleText,    
                 author: {
