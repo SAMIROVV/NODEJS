8eaa287772a65055df04edc71c3062566a107b41
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-02 15:04:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -411,13 +411,14 @@ https.get(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("
 app.get('/YTSCRAPER0', (req0, res0) => {
 const miniget = require('miniget');
 
-const response =  miniget(      'https://www.youtube.com/results?search_query=goo'   ) .text();
-    const line = response.match(/window\["ytInitialData"]\s*=\s*(.*);+\n/)[0];
-    const json = JSON.parse(line.substring(line.indexOf('{'), line.length - 2));
-    const result = json['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']
-        ['contents'][0]['itemSectionRenderer']['contents'];
+
 	
-	
+	const response = miniget('https://www.youtube.com/results?search_query=goo' ).text()
+	const line = response.match(/window\["ytInitialData"]\s*=\s*(.*);+\n/)[0]
+        const json = JSON.parse(line.substring(line.indexOf('{'), line.length - 2))
+        const result = json['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']
+                       ['contents'][0]['itemSectionRenderer']['contents']
+    
 	
 	
 
