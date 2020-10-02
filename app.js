286ce772559bf6287a4080ae61ba7ecfbc8d1c1f
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-02 19:41:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -483,7 +483,7 @@ duration :  data['lengthText']['simpleText'],
 			    
 			    res0.header('Content-Type', 'application/json').send(kind.map(function (item) {  return 
 								 item.type+'☉'+item.identifier+'☉'+item.uri+'☉'+
-								 item.title+'☉'+item.author													  +'☉'+item.thumbnails    item.description
+								 item.title+'☉'+item.author+'☉'+item.thumbnails+'☉'+item.description
 				                                 +'☉'+item.publishedTime+'☉'+item.viewCount+'☉'+item.duration				  
 													  })    );
 				      });});
