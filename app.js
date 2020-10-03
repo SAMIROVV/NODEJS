07fc7be584ac7a2f819bc81181ea329d6e75520a
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-03 01:58:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -583,7 +583,7 @@ duration :  data['lengthText'],//.simpleText,
 	
 	
 //second pages
-https.get('https://www.youtube.com/results?search_query=girl&sp=EgIQAQ%253D%253D'  , (res) => {  
+https.get(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/YTSCRAPER0?','')  +'&page=2'  , (res) => {  
                     let data = '';
                     res.on('data', (chunk) => {data += chunk; });
                     res.on('end', () => {
@@ -657,7 +657,7 @@ else return {
 			    
 			    
 			      			    
-			    res0.header('Content-Type', 'application/json').send(datapage1+'💚'+datapage2);console.log(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("☆")[0].replace('/YTSCRAPER0?','')  +'&page=2')
+			    res0.header('Content-Type', 'application/json').send(datapage1+'💚'+datapage2)
 				      });});});});
 
 		    
