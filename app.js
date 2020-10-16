ecbeae5478779b0e10b0211274ab82341725cb24
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-16 13:21:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 	     
 	     
 	  
-var urls = ['https://adrianmejia.com/atom.xml', 'https://twitrss.me/twitter_user_to_rss/?user=amejiarosario'];
+var urls = ['https://trends.google.com/trends/api/autocomplete/sexy?hl=en-US&tz=-60', 'https://trends.google.com/trends/api/autocomplete/bitcoin?hl=en-US&tz=-60'];
 	     //var urls = [encodeURI(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("■")[0].split("☆")[0].replace('/SCRAPER10?','')), encodeURI(decodeURIComponent(url.format({ pathname: req0.originalUrl })).split("■")[1].split("☆")[0].replace('/SCRAPER10?',''))];
 
 var completed_requests = 0;
