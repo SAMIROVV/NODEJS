ec73dbaaf77a0ee7107ce546143d3300162b7165
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-17 23:53:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -107,12 +107,12 @@ urls.forEach(function(url) {
     res.on('data', function(chunk){     responses.push(chunk);    });
 
     res.on('end', function(){
-      if (completed_requests++ == urls.length) { 
+      //if (completed_requests++ == urls.length) { 
 	    var clean = JSON.parse(responses.join().slice(5)).default.topics.map(function (item) {  return item.title+'☔☉'+item.type+'💙💔'})
 	     
 	    
 	    console.log('body:', clean.join());  res0.json(clean.join());
-      }      
+      //}      
     });
   });
 });
