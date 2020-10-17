39c01e72d7f1f4c6246413eb31f78db3743995bb
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-17 23:28:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -103,19 +103,19 @@ var completed_requests = 0;
 
 urls.forEach(function(url) {
   var responses = [];
-  https.get(url, function(res) {
+  const requests = https.get(url, function(res) {
     res.on('data', function(chunk){     responses.push(chunk);    });
 
     res.on('end', function(){
       //if (completed_requests++ == urls.length - 2) { 
 	    var clean = JSON.parse(responses.join().slice(5)).default.topics.map(function (item) {  return item.title+'☔☉'+item.type+'💙💔'})
 	     
-	    Promise.all(clean.join()).then(values => {   console.log(values);  });
+	    //Promise.all(clean.join()).then(values => {   console.log(values); res0.send(values) });
 	    
 	    //console.log('body:', clean.join());  res0.json(clean.join());
       //}      
     });
-  });
+  });return Promise.all(requests)
 });
 	     
 	     
