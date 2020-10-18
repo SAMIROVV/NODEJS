addb8c65083ca5f26996927eb231471f2c68ed61
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-18 02:07:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ urls.map(function(url) {
     res.on('end', function(){
       //if (completed_requests == urls.length) { 
 	    var clean = JSON.parse(responses.join().slice(5)).default.topics.map(function (item) {  return item.title+'☔☉'+item.type+'💙💔'})
-	     
+	     return clean;
 	    
 	    console.log('body:', clean.join());  //res0.json(clean.join());
       //}      
