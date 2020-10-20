37bf11b89e80a6ccfed45bccf29a1fb35b5bcc2a
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-20 19:59:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -65,11 +65,11 @@ for (i in urls) {
 										      responses.push(urls[i].split('🔸')[1]+'💚'+unescapeHTML(clean));
 	      }
 	    
-	           if(type === 'https://trends.google.com/trends/api/dailytrends?'){ var clean  = JSON.parse(data.slice(5)).default.trendingSearchesDays[0].trendingSearches.map(function (item) {  return '🐸🐲'+item.title.query+'☔☉'+item.formattedTraffic+'☔☉'+item.relatedQueries.map(function (item) {  return item.query+'💙💔'})+'☔☉'+item.articles.map(function (item) {  return item.title+'🔹🍎'+item.timeAgo+'🔹🍎'+item.snippet+'💙💔'})   }) ;
+	           if(type === 'https://trends.google.com/trends/api/dailytrends?'){ var clean  = JSON.parse(data.slice(5)).default.trendingSearchesDays[0].trendingSearches.map(function (item) {  return '🐸🐲'+item.title.query+'☔☉'+item.formattedTraffic+'☔☉'+item.relatedQueries.map(function (item) {  return item.query}).join('💙💔')+'☔☉'+item.articles.map(function (item) {  return item.title+'🔹🍎'+item.timeAgo+'🔹🍎'+item.snippet}).join('💙💔')   }) ;
 										      responses.push(urls[i].split('🔸')[1]+'💚'+unescapeHTML(clean));
 	      }
 	    
-	           if(type === 'https://trends.google.com/trends/api/realtimetrends?'){ var clean  = JSON.parse(data.slice(5)).storySummaries.trendingStories.map(function (item) {  return item.articles.map(function (item) {  return item.articleTitle+'☔☉'+item.source+'☔☉'+item.time+'☔☉'+item.snippet}).join('💙💔')})       		
+	           if(type === 'https://trends.google.com/trends/api/realtimetrends?'){ var clean  = JSON.parse(data.slice(5)).storySummaries.trendingStories.map(function (item) {  return item.articles.map(function (item) {  return item.articleTitle+'☔☉'+item.source+'☔☉'+item.time.replace('il y a','')+'☔☉'+item.snippet}).join('💙💔')})       		
 										      responses.push(urls[i].split('🔸')[1]+'💚'+unescapeHTML(clean));
 	      }   
 	    
