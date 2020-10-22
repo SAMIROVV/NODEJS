d0aa90a7f349a85cd92f6da39878a5f355a6620d
SAMIROVV <70457295+SAMIROVV@users.noreply.github.com>
2020-10-22 15:52:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -228,10 +228,10 @@ for (i in urls) {
 											      if (completed_requests == urls.length) { res0.send(responses);  }
 	      }   
 	    
-        if(type === 'https://trends.google.com/trends/api/explore?')         { var cookie = res.headers['set-cookie'][0].split(';')[0];console.log(cookie)	
+        if(type === 'https://trends.google.com/trends/api/explore?')         { var cookie = res.headers['set-cookie'][0].split(';')[0];console.log(cookie);	
 									      
-									      	    mynpm.get({url:encodeURI(urls[i].split('🔸')[0]) , method: 'GET', header: { 'Cookie': cookie } }, (res) => {
-                                                                                                var td = JSON.parse(data.slice(4)).widgets.map(function (item) {  return item.id.replace('_0','').replace('_1','').replace('_2','').replace('_3','').replace('_4','')+'☉'+JSON.stringify(item.request)+'☉'+item.token})   ;
+									      	    mynpm({url:encodeURI(urls[i].split('🔸')[0]) , method: 'GET', header: { 'Cookie': cookie } }, (err, res, body) => {
+                                                                                                var td = JSON.parse(body.slice(4)).widgets.map(function (item) {  return item.id.replace('_0','').replace('_1','').replace('_2','').replace('_3','').replace('_4','')+'☉'+JSON.stringify(item.request)+'☉'+item.token})   ;
 											     
                                                                                                                        if(kind === 'SCPGTEXPLORE' || kind === ''){ 
 															       responses.push(urls[i].split('🔸')[1]+'💚'+td);
